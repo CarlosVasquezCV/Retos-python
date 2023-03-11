833804541dca56c30635fff53ed1a58b75b423f7
CarlosVasquezCV <125754625+CarlosVasquezCV@users.noreply.github.com>
2023-03-11 08:16:20 -0500

    Add files via upload

---
+++ 1er RETO.py
@@ -0,0 +1,6 @@
+print ("Hola, abuelita... ")
+
+gradof= float (input("Ingresar valor en farenheit: " ))
+
+gradosC= (gradof-32)/1.8
+gradosC= float (print("El valor en grados Centigrados es: ",gradosC))
---
+++ 2do RETO.py
@@ -0,0 +1,10 @@
+print ("Hola, enfermera...")
+
+pesobb= float(input("Ingrese el peso del bebé: " ))
+total1 = pesobb + 10
+mesesbb=float(input("Ingresa los meses de nacido del bebé: "))
+total2 = mesesbb * 10 
+total1_2 = total1/total2
+
+dosis = total1_2 * 8
+dosisVacuna = print ("bb =",dosis)
